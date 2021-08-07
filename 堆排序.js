0a9453d681c214cc7fab867ba28c8bc1430c5556
zystudios <zystudios@gmail.com>
2021-08-07 21:55:18 +0800

    桶排序

---
+++ 堆排序.js
@@ -0,0 +1,194 @@
+// 数组交换
+function swap(arr, i, j) {
+  // 把数组里面 i位置和j位置交换
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+// 随机生成一个数组，做测试用
+function randomArr(n = 20) {
+  let arr = [];
+  for (var i = 0; i < n; i++) {
+    arr.push(Math.round(Math.random() * 100));
+  }
+  return arr;
+}
+
+/*
+ 满二叉树，是完全二叉树
+
+ 完全二叉树，就是一个二叉树，从左到右，顺序上不缺
+
+     1
+   2   3
+ 4   5
+ 
+ 是一个完全二叉树，从左到右，位置不缺
+
+     1
+   2   4
+ 3   5 
+ 
+ 不是完全二叉树，因为2的右孩子缺了
+
+
+公式
+
+完全二叉树， 知道某个位置i，左孩子 2i+1   右孩子 2i+2  父节点：(i-1) / 2 直接取证
+
+比如  下面的完全二叉树，用位置表示
+
+       0
+   1       2
+ 3   4   5
+ 
+ 1 位置的左孩子位置  2x1 + 1 = 3
+ 1 位置的右孩子位置  2x1 + 2 = 4
+ 1 位置父节点 (1-1) / 2 = 0
+
+ 注意，上面012345 都是位置，非数值，在数组里可以用一个连续空间表示
+
+ [2，3，6，1，7，8]
+  0，1，2，3，4，5
+
+  如果用实际数值表示就是
+
+       2
+   3       6
+ 1   7   8
+ 
+ 数字3的左孩子是几？ 数字3的位置是1，2x1+1 = 3，3位置的数字是1
+ 以此类推
+
+
+ 堆：特殊的完全二叉树
+ 所以堆肯定是完全二叉树
+
+ 堆，分 大根堆 小根堆
+
+ 大根堆：完全二叉树里，每一个节点的子树，他的所有节点都小于这个子树根节点
+
+ 小根堆：完全二叉树里，每一个节点的子树，他的所有节点都大于这个子树根节点
+
+ 换句话说，大根堆，头结点在子树里最大，小根堆，头结点在子树里最小
+
+*/
+
+// 堆排序
+// 时间复杂度 logN
+
+//不管是向上比较 heapInsert  还是向下比较 heapify，他只是一条路径，不会管他兄弟的路径，所以是logN
+
+function heapInsert(arr, currentIndex) {
+  // 判断和父节点哪个大
+  var fatherIndex = Math.floor((currentIndex - 1) / 2);
+  // 当前的父节点，必须大于0，等于0说明已经是根节点了
+  while (arr[currentIndex] > arr[fatherIndex]) {
+    // 当前节点的值，和他父节点比较大小，如果大于父节点，那么就交换
+    swap(arr, currentIndex, fatherIndex);
+    // 交换完位置以后，当前节点位置就指向父节点了，因为还要和父节点的父节点比较大小，一步步向根节点迈进，确保这个最大值一直向上交换
+    currentIndex = fatherIndex;
+    // 这个时候父节点就是之前父节点的父节点了
+    // 这是一个从下到上的过程
+    fatherIndex = Math.floor((currentIndex - 1) / 2);
+  }
+}
+
+// // 测试一下，比如，把这个二叉树，整理成大根堆
+// var testArr = [2, 3, 4, 7, 1, 8, 9, 0];
+// var arr = [];
+// for (var i = 0; i < testArr.length; i++) {
+//   arr.push(testArr[i]);
+//   heapInsert(arr, i);
+// }
+// console.log(arr);
+
+// 堆化，什么意思，就是比较每个节点的左右孩子，如果节点值大于左右孩子里最大的，停止
+// 如果节点值 小于左孩子或者右孩子里面最大的，节点和这个最大的交换
+// 然后继续向下，直到某个节点没有孩子，或者只有一个孩子
+function heapify(arr, currentIndex, heapsize) {
+  // 左孩子，右孩子的位置
+  var leftChild = Math.floor(currentIndex * 2 + 1);
+  var rightChild = Math.floor(currentIndex * 2 + 2);
+
+  while (leftChild < heapsize) {
+    var lagerIndex = 0;
+    // 如果右孩子存在，并且，右孩子数值大于左孩子数值
+    if (rightChild < heapsize && arr[rightChild] > arr[leftChild]) {
+      lagerIndex = rightChild;
+    } else {
+      lagerIndex = leftChild;
+    }
+
+    // 这个时候，largeIndex就是 左孩子和右孩子里面，比较大的那个位置
+    // 然后和父比较,注意，这个时候，currentIndex 就是父节点
+
+    if (arr[lagerIndex] > arr[currentIndex]) {
+      largeIndex = lagerIndex;
+    } else {
+      largeIndex = currentIndex;
+    }
+
+    // 这个时候，largeIndex 就是 父，左，右，三个里面最大的那个位置了
+
+    if (largeIndex == currentIndex) {
+      // 如果最大的这个就是父本身，那么结束循环
+      break;
+    }
+
+    // 父节点和large交换，这时候，large要么是左孩子，要么是右孩子
+
+    swap(arr, largeIndex, currentIndex);
+
+    // 交换完以后，更新节点。largeIndex本身就是孩子节点
+    // 左孩子节点就是那个比较大的large的左孩子
+    // 右孩子就是那个比较大的large的右孩子
+    // currentIndex 之前就是父节点，这个时候等于largeindex，就是说要开始比较这个孩子的左右孩子了
+    // 这是一个从上到下的过程
+
+    currentIndex = largeIndex;
+
+    leftChild = Math.floor(largeIndex * 2 + 1);
+    rightChild = Math.floor(largeIndex * 2 + 2);
+  }
+}
+
+// 堆排序开始
+// 首先，任何一个数组，先变成大根堆
+
+var testArr = randomArr(20);
+
+for (var i = 0; i < testArr.length; i++) {
+  heapInsert(testArr, i);
+}
+
+// 上面就变成大根堆了
+
+var heapsize = testArr.length;
+
+// 然后，就是开始向下比较了
+while (heapsize > 0) {
+  heapify(testArr, 0, heapsize);
+  // heapify以后，数组第一个就是最大数了，这个时候，第一个和最后一个交换
+  swap(testArr, 0, heapsize - 1);
+  // 交换以后，heapsize -- ，然后就不在比较这个最大数了，比较他前面的数组，重复，直到剩最右一个数
+  heapsize--;
+}
+
+console.log(testArr);
+
+/*
+
+改进算法，上面把一个数组先转为大根堆，是一个个进行的方式
+其实有个更好的办法，直接把这个数组 heapify 就可以
+heapify 从上向下，比较孩子
+会稍微快那么一点点
+*/
+
+var testArr2 = randomArr(10);
+for (var i = testArr2.length - 1; i >= 0; i--) {
+  heapify(testArr2, i, testArr2.length);
+}
+
+console.log(testArr2);
---
+++ 桶排序.js
@@ -0,0 +1,154 @@
+/*
+
+之前的排序，快排，插入，冒泡，选择，堆，归并，都是基于比较的排序
+说白了就是需要比较两个数大小，桶排序不需要比较大小
+他是根据词频来排序
+
+优点：时间复杂度O（N）
+缺点，比如要知道数字的范围，而且范围不能特别大
+
+举例子：一个数组，里面最小的数字是5，最大的数字是45，排序
+
+首先，申请数组空间，0-45
+
+
+*/
+
+function tong(arr) {
+  var temparr = [];
+  for (var i = 0; i < 45; i++) {
+    temparr[i] = 0;
+  }
+  // 因为js数组比较灵魂，且长度随时可变，这里就模拟声明一个长度45的数组，并且0填充
+
+  for (var i = 0; i < arr.length; i++) {
+    var temp = arr[i];
+    if (temparr[temp]) {
+      temparr[temp]++;
+    } else {
+      temparr[temp] = 1;
+    }
+  }
+
+  console.log(temparr.toString());
+}
+
+//tong([5, 7, 9, 12, 22, 31, 7, 18, 45]);
+/*
+
+然后这个数组就变成了，下标是具体数，数组值是出现的次数，也就是词频
+那么，遍历一次，就会得到排序好的结果
+0,0,0,0,0,1,0,2,0,1,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1
+
+5出现1次，7出现2次，9一次。。。。
+
+按照这个下标顺序，就是 5，7，7，9，12，18，22，31，45
+
+这个是最基本的桶排序，利用词频计数，也叫计数排序
+
+但是他限制很大，就是要排序的范围不能太大，比如你一个数组，最小的是0，最大20个亿
+那么你数组空间就是20个亿，会极大的浪费
+
+*/
+
+function tong1(arr) {
+  // 假设我们需要排序一个整数的数组，整数都是10进制的
+  // 我们准备10个桶，为什么是10个，因为十进制数，0-9一共10个
+  // 桶只是一个形像的说法，就是容器，可以是数组，队列，列表，栈，等等
+
+  var tongArr = [[], [], [], [], [], [], [], [], [], []];
+  // 我们用二维数组模拟一下，这10个桶
+  for (var i = 0; i < arr.length; i++) {
+    var geweishu = (arr[i] / 1) % 10;
+    // 取个位数
+    tongArr[geweishu].push(arr[i]);
+  }
+  // 首先，先根据个位数，放入桶里，放完以后，倒出来
+  arr = [];
+
+  for (var j = 0; j < tongArr.length; j++) {
+    if (tongArr[j].length > 0) {
+      // 判断下桶是不是空
+      for (var k = 0; k < tongArr[j].length; k++) {
+        arr.push(tongArr[j][k]);
+      }
+    }
+  }
+
+  // 这样，经过上面一轮，原数组里，按照数字个位数，排序已经好了
+
+  // 下面，桶重置一下，按照十位数接着来
+  tongArr = [[], [], [], [], [], [], [], [], [], []];
+
+  for (var i = 0; i < arr.length; i++) {
+    var shiweishu = Math.trunc((arr[i] / 10) % 10);
+    // 取十位数
+    tongArr[shiweishu].push(arr[i]);
+  }
+
+  arr = [];
+  for (var j = 0; j < tongArr.length; j++) {
+    if (tongArr[j].length > 0) {
+      // 判断下桶是不是空
+      for (var k = 0; k < tongArr[j].length; k++) {
+        arr.push(tongArr[j][k]);
+      }
+    }
+  }
+
+  // 这样，十位数也排序完了，因为我们这个原始数组里，最大的是111，是个三位数，所以还有按照百位再来一次
+
+  tongArr = [[], [], [], [], [], [], [], [], [], []];
+
+  for (var i = 0; i < arr.length; i++) {
+    var baiweishu = Math.trunc((arr[i] / 100) % 10);
+    // 取百位数
+    tongArr[baiweishu].push(arr[i]);
+  }
+
+  // 总结一个技巧，一个数组取个位十位百位等 n / 位数（个位1，十位10，百位100，千位1000...） % 10，
+
+  arr = [];
+  for (var j = 0; j < tongArr.length; j++) {
+    if (tongArr[j].length > 0) {
+      // 判断下桶是不是空
+      for (var k = 0; k < tongArr[j].length; k++) {
+        arr.push(tongArr[j][k]);
+      }
+    }
+  }
+
+  // 这里，就完成了，因为最大是三位数，如果有四位数，那就千位再来一次，以此类推
+
+  console.log(arr);
+}
+
+//tong1([12, 23, 4, 111, 67, 45, 19]);
+
+// 上面的过程有点繁琐，我们简化一下
+
+function tong2(arr, maxWeishu) {
+  var tongArr = [[], [], [], [], [], [], [], [], [], []];
+  for (var i = 0; i < maxWeishu; i++) {
+    tongArr = [[], [], [], [], [], [], [], [], [], []];
+
+    for (var j = 0; j < arr.length; j++) {
+      var weishu = Math.trunc((arr[j] / Math.pow(10, i)) % 10);
+      // 取位数,注意这个小技巧 n / 位数（个位1 十位10，百位100...） % 10
+      tongArr[weishu].push(arr[j]);
+    }
+
+    arr = [];
+    for (var m = 0; m < tongArr.length; m++) {
+      if (tongArr[m].length > 0) {
+        // 判断下桶是不是空
+        for (var n = 0; n < tongArr[m].length; n++) {
+          arr.push(tongArr[m][n]);
+        }
+      }
+    }
+  }
+  console.log(arr);
+}
+
+tong2([25, 17, 86, 6, 1, 123, 245, 278, 891, 1, 3, 12, 6, 8], 3);
